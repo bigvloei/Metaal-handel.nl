adaa79d5d4e29848b212f5a33c3e5fe8c52a458f
bigvloei <jouw-email@voorbeeld.com>
2025-03-04 03:45:01 +0000

    Automatische update op Tue Mar  4 03:45:01 UTC 2025

--- index.php
+++ index.php
@@ -13,8 +13,7 @@ define('TEMPLATES_PATH', PRIVATE_PATH . '/templates');
 define('LANGUAGES_PATH', PRIVATE_PATH . '/languages');
 
 // Laad configuratie
-require_once CONFIG_PATH . '/db_config.php';
-
+require_once(__DIR__ . '/../private/config/db_config.php');
 // Laad algemene functies
 require_once INCLUDES_PATH . '/functions.php';
 $page_title = "Het platform voor het ophalen van oud ijzer en metalen";
